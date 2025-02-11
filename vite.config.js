de2dc7d3f49ad0f2b4e62802e25317da9c94a23a
200_de_salame <122568719+pablodamianr1983@users.noreply.github.com>
2025-02-11 00:01:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-  'import.meta.env.VITE_API_URL': JSON.stringify('https://sanjose-back-production.up.railway.app')
-},
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://sanjose-back-production.up.railway.app') // Definir URL directa de la API
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'https://sanjose-back-production.up.railway.app', // ✅ Asegúrate de incluir HTTPS
+        target: 'https://sanjose-back-production.up.railway.app', // Asegúrate de incluir HTTPS
         changeOrigin: true,
         secure: true
       }
