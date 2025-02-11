199490c80d80b2bd9de207e95e9cea9dd422a01c
200_de_salame <122568719+pablodamianr1983@users.noreply.github.com>
2025-02-10 22:43:11 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',  // URL de tu API
+        target: 'https://tu-backend.railway.app',  // URL de tu API
         changeOrigin: true,  // Cambia el origen de la solicitud al destino
         secure: false       // Permite conexiones no seguras (HTTP)
       }
