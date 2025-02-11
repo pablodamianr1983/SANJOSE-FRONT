de50cde75de99ddb8af30d0a2c730cfd05eb49c2
200_de_salame <122568719+pablodamianr1983@users.noreply.github.com>
2025-02-10 23:26:38 -0300

    Update apiClient.js

--- src/apiClient.js
+++ src/apiClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.VITE_API_URL || 'https://sanjose-back-production.up.railway.app', // ✅ Usa la URL del backend en producción
+  baseURL: import.meta.env.VITE_API_URL || 'https://sanjose-back-production.up.railway.app',
   withCredentials: true
 });
 
